7cb1ff5d453b5cf944cebb456cb2e9428a57c59c
leadoflife <134975344+leadoflife@users.noreply.github.com>
2023-06-19 10:08:43 -0400

    chore: recreate test results directory
    Signed-off-by: leadoflife <134975344+leadoflife@users.noreply.github.com>

--- src/apps/leadof-us/check-test.sh
+++ src/apps/leadof-us/check-test.sh
@@ -38,6 +38,9 @@ test() {
   echo "Copying output files from container \"${image_name}\"..."
   # copy output files from container
   podman run --name ${image_name} --detach ${image_tag} sleep 1000
+  if [ -d "./test-results/${target_name}/" ]; then
+    rm -rf ./test-results/${target_name}/
+  fi
   mkdir -p ./test-results/${target_name}/
   podman cp ${image_name}:/usr/src/test-results/ ./test-results/${target_name}/
   mv ./test-results/${target_name}/test-results/* ./test-results/${target_name}/
